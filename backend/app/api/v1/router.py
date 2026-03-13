902a8136aca3c7d40f1909165b4a783c3f4cecaf
Ishan Singh <ishan18190@gmail.com>
2026-03-13 16:25:00 +0530

    Update router.py

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-
 from app.api.v1.endpoints.stocks import router as stocks_router
-
+from app.api.v1.endpoints.auth import router as auth_router
+from app.api.v1.endpoints.notify import router as notify_router
 
 api_router = APIRouter()
 api_router.include_router(stocks_router)
+api_router.include_router(auth_router)
+api_router.include_router(notify_router)
